70375a0e204d32e241b628779bc0eef03b2b9ed3
Ingo Kuba <ikuba@intension.de>
2019-05-21 19:52:05 +0200

    8: Order shirt configuration.

--- wolpertinger-vue/app/src/components/Main.vue
+++ wolpertinger-vue/app/src/components/Main.vue
@@ -10,7 +10,7 @@
                 <drop @drop="handleDrop">
                   <v-img
                     id="tertiary1"
-                    v-bind:src="configuration.tertiary1"
+                    v-bind:src="configuration.tertiary1.high"
                     aspect-ratio="1"
                     @click="setUrlParam($event.currentTarget.id, null)"
                   ></v-img>
@@ -22,7 +22,7 @@
                 <drop @drop="handleDrop">
                   <v-img
                     id="main"
-                    v-bind:src="configuration.main"
+                    v-bind:src="configuration.main.high"
                     aspect-ratio="1"
                     @click="setUrlParam($event.currentTarget.id, null)"
                   ></v-img>
@@ -34,7 +34,7 @@
                 <drop @drop="handleDrop">
                   <v-img
                     id="tertiary2"
-                    v-bind:src="configuration.tertiary2"
+                    v-bind:src="configuration.tertiary2.high"
                     aspect-ratio="1"
                     @click="setUrlParam($event.currentTarget.id, null)"
                   ></v-img>
@@ -48,7 +48,7 @@
                 <drop @drop="handleDrop">
                   <v-img
                     id="secondary1"
-                    v-bind:src="configuration.secondary1"
+                    v-bind:src="configuration.secondary1.high"
                     aspect-ratio="1"
                     @click="setUrlParam($event.currentTarget.id, null)"
                   ></v-img>
@@ -60,7 +60,7 @@
                 <drop @drop="handleDrop">
                   <v-img
                     id="secondary2"
-                    v-bind:src="configuration.secondary2"
+                    v-bind:src="configuration.secondary2.high"
                     aspect-ratio="1"
                     @click="setUrlParam($event.currentTarget.id, null)"
                   ></v-img>
@@ -74,7 +74,7 @@
                 <drop @drop="handleDrop">
                   <v-img
                     id="tertiary3"
-                    v-bind:src="configuration.tertiary3"
+                    v-bind:src="configuration.tertiary3.high"
                     aspect-ratio="1"
                     @click="setUrlParam($event.currentTarget.id, null)"
                   ></v-img>
@@ -83,6 +83,30 @@
             </v-flex>
           </v-layout>
         </v-img>
+        <v-dialog v-model="orderDialog" persistent max-width="600px">
+          <template v-slot:activator="{on}">
+            <v-btn color="success" :disabled="!button.active" block v-on="on">Place order</v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">Order</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container grid-list-md>
+                <v-layout wrap>
+                  <v-flex xs12 sm6 md4>
+                    <v-text-field id="orderer" label="Name" required></v-text-field>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" flat @click="orderDialog = false">Cancel</v-btn>
+              <v-btn color="blue darken-1" flat @click="order">Order</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-flex>
       <v-flex xs6 pa-3>
         <v-layout row wrap>
@@ -113,13 +137,25 @@ export default {
     return {
       circle: circle,
       imageService: process.env.VUE_APP_BACKEND_URL + "images",
+      orderService: process.env.VUE_APP_BACKEND_URL + "orders",
       images: [],
-      configuration: [],
+      configuration: {
+        main: "",
+        secondary1: "",
+        secondary2: "",
+        tertiary1: "",
+        tertiary2: "",
+        tertiary3: ""
+      },
       snackbar: {
         duration: 1500,
         show: false,
         text: "nothing"
-      }
+      },
+      button: {
+        active: false
+      },
+      orderDialog: false
     };
   },
   created() {
@@ -130,36 +166,65 @@ export default {
         this.configuration = this.loadConfiguration(this.images);
       })
       .catch(error => {
-        var message = error.message;
-        // Error
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          message = error.response.data;
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          message = error.request;
-        }
-        this.showSnackbar(message, 10000);
+        this.handleError(error);
       });
   },
   methods: {
+    handleError(error) {
+      var message = error.message;
+      // Error
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        const data = error.response.data;
+        message = data.errors
+          ? this.formatErrors(data.errors)
+          : JSON.stringify(data, null, 2);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        message = error.request;
+      }
+      this.$dialog.error({
+        text: message
+      });
+    },
+    /**
+     * Format the errors to nice html.
+     */
+    formatErrors(errors) {
+      var message = "";
+      for (var i = 0; i < errors.length; i++) {
+        var error = errors[i];
+        message += error.message + "\n";
+      }
+      return message;
+    },
     /**
      * Display shirt configuration via url params.
      */
     loadConfiguration(images) {
       const context = this;
-      var configuration = [];
+      var configuration = {
+        main: "",
+        secondary1: "",
+        secondary2: "",
+        tertiary1: "",
+        tertiary2: "",
+        tertiary3: ""
+      };
+      var size = 0;
       const params = new URLSearchParams(window.location.search);
       params.forEach(function(value, key) {
         var image = context.getImage(images, value);
-        if (!image || context.contains(configuration, image.high)) {
+        if (!image || context.contains(configuration, image)) {
           window.location.replace(location.pathname);
         }
-        configuration[key] = image.high;
+        configuration[key] = image;
+        size++;
       });
+      context.button.active = size == 6;
       return configuration;
     },
     getImage(images, name) {
@@ -176,7 +241,7 @@ export default {
         // dragged image unknown
         return;
       }
-      if (this.contains(this.configuration, image.high)) {
+      if (this.contains(this.configuration, image)) {
         // image already on shirt
         this.showSnackbar("Image already on the shirt.", 1500);
         return;
@@ -226,6 +291,52 @@ export default {
       this.snackbar.text = text;
       this.snackbar.duration = duration;
       this.snackbar.show = true;
+    },
+    order() {
+      const order = {
+        orderer: document.getElementById("orderer").value,
+        visible: true,
+        images: [
+          {
+            level: 1,
+            image: this.configuration.main
+          },
+          {
+            level: 2,
+            image: this.configuration.secondary1
+          },
+          {
+            level: 3,
+            image: this.configuration.secondary2
+          },
+          {
+            level: 4,
+            image: this.configuration.tertiary1
+          },
+          {
+            level: 5,
+            image: this.configuration.tertiary2
+          },
+          {
+            level: 6,
+            image: this.configuration.tertiary3
+          }
+        ]
+      };
+      axios
+        .post(this.orderService, order)
+        .then(response => {
+          if (response.status == 201) {
+            this.showSnackbar(
+              "Your Shirt has successfully been ordered!",
+              5000
+            );
+          }
+        })
+        .catch(error => {
+          this.handleError(error);
+        });
+      this.orderDialog = false;
     }
   }
 };
--- wolpertinger-vue/app/src/main.js
+++ wolpertinger-vue/app/src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vue from "vue";
+import App from "./App.vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import VuetifyDialog from "vuetify-dialog";
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
+Vue.use(VuetifyDialog);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-    render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
